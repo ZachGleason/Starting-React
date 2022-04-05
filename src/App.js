fca5f9036d47ecb51fa964ca80a98068c8ebdea5
Zach <zgleason37@gmail.com>
2022-04-05 12:10:42 -0700

    todo list

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 function App() {
   return (
     <>
-    <form action="" method='post' id='send'>
-      <input type="text" placeholder='Name' name='name'/>
-      <input type="email" placeholder='Email' name='email'/>
-      <input type="password" placeholder='Password' name='password'/>
-      <button> Submit</button>
-    </form>
+    <h1 id='header'>Hello Dojo!</h1>
+    <p id='paragraph'>Things I need to do</p>
+    <ul className='text'>
+      <li className='text'>Learn React</li>
+      <li className='text'>Climb Mt. Everest</li>
+      <li className='text'>Run a Marathon</li>
+      <li className='text'>Feed the Doggo</li>
+    </ul>
     </>
   );
 }
