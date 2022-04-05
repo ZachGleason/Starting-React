b85b5b943f8ad30ef6c4efd842bdb8de3817f6ca
Zach <zgleason37@gmail.com>
2022-04-05 12:03:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form action="" method='post' id='send'>
+      <input type="text" placeholder='Name' name='name'/>
+      <input type="email" placeholder='Email' name='email'/>
+      <input type="password" placeholder='Password' name='password'/>
+      <button> Submit</button>
+    </form>
+    </>
   );
 }
 
